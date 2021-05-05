5363153bfdfa10ef816d7fad7b5bd9d7f93742dd
chengqj <wowcheng163@163.com>
2021-05-05 17:03:42 +0800

    update

---
+++ 00-springboot-senior/src/main/java/com/chengqj/springbootsenior/tcp/util/LogUtil.java
@@ -0,0 +1,13 @@
+package com.chengqj.springbootsenior.tcp.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author chengqiujing
+ * @Date 2020/6/12 15:04
+ * @Desc
+ */
+public class LogUtil {
+    public static final Logger LOGGER = LoggerFactory.getLogger(LogUtil.class);
+}
